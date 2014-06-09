10f5d796a125915c2fe8a03cf569b9824cb413c5
Whien <sal95610@gmail.com>
2014-06-09 20:25:42 +0800

    身為商家的管理者，我要能夠在後台上架我的東西，並且販賣

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+    resources :products
+  end
+
+  #namespace
+  #要把admin功能關起來，所以用namespace來設定。
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
