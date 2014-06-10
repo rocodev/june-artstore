a74f1319c443cafaa3d494b401f7b1cd35863f48
humanhighway <humanhighway@gmail.com>
2014-06-10 10:50:25 +0800

    add admin and product acitons implement controller functions and views

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,9 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  namespace :admin do
+    resources :products
+  end
+
 end
