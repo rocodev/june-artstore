2cc951fad5526c5356968e4c18cc94c09d9a60bb
sdlong <sdlong.jeng@gmail.com>
2014-06-11 02:46:15 +0800

    index page done

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
+
+  root to: "pages#welcome"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
