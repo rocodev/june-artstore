182b3ec7f75b14b9ea69fb03079c8ed383ba7923
orga <orga.shih@gmail.com>
2014-06-12 23:20:04 +0800

    adm/product: modify layout of show page

--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -1,7 +1,7 @@
 module Admin::ProductsHelper
   def products_render_image
     if @product.photos.first != nil
-      image_tag @product.photos.first.image.url
+      image_tag @product.photos.first.image.url, :class => "img-rounded"
     else
       "No Pic"
     end
