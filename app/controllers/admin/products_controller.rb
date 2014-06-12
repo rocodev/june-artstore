26c7c0d8ed65d15ca7d2e225f3aebe61de59a48e
Szu <szu@Szu-Air.local>
2014-06-12 23:43:19 +0800

    Add index.html but can not show quantity

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,28 @@
+class Admin::ProductsController < ApplicationController
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+
+		if @product.save
+			redirect_to admin_products_path
+		else
+			render :new
+		end
+	end
+
+	def index
+		@products = Product.all
+	end
+
+    
+	private  #private behind this line 
+
+	def product_params
+		params.require(:product).permit(:title, :description, :quanttity)
+	end
+
+
+end
