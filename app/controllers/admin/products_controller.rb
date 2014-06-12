d3539ab5c14beb58cc5c45de17bb757dcb75a1c7
Vincent Chen <vince78718@gmail.com>
2014-06-12 21:53:10 +0800

    Finish basic CRUD of product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,8 +1,58 @@
 class Admin::ProductsController < ApplicationController
 
+	# Using the main layout of admin
+	layout 'admin/main'
+	
 	# Index of Admin::product
 	def index
+		@products = Product.all
+	end
+
+	def new
+		@product = Product.new
 
 	end
 
+	def create
+		@product = Product.new(product_params)
+
+		if @product.save
+			redirect_to admin_products_path
+		else
+			render :new
+		end
+	end
+
+	def show
+		@product = Product.find(params[:id])
+	end
+
+	def edit
+		@product = Product.find(params[:id])
+	end
+
+	def update
+		@product = Product.find(params[:id])
+
+		if @product.update(product_params)
+			redirect_to admin_products_path(@product)
+		else
+			render :edit
+		end
+
+	end
+
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+
+		redirect_to admin_products_path
+
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:name, :description, :quantity, :price)
+	end
 end
