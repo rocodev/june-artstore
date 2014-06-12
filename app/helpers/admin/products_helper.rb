f5846e1dd18adb32a2e3705477c3a2c18b97ee48
orga <orga.shih@gmail.com>
2014-06-12 21:57:26 +0800

    admin/product: fix show page error, if product without photo

--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -1,2 +1,9 @@
 module Admin::ProductsHelper
+  def products_render_image
+    if @product.photos.first != nil
+      image_tag @product.photos.first.image.url
+    else
+      "No Pic"
+    end
+  end
 end
