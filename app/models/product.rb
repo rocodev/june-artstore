b64a1291adbb9b6bce164de8e18d9eafa2a49a7f
humanhighway <humanhighway@gmail.com>
2014-06-12 10:47:16 +0800

    remove unused property

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,5 @@ class Product < ActiveRecord::Base
   validates :title, presence: :true
 
   has_many :photos, :dependent => :destroy
-  has_one :image, :class_name => "Photo"
   accepts_nested_attributes_for :photos
 end
