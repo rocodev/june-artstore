0aa27db10adbe9c495c08bfc9b82c527eaac6f68
orga <orga.shih@gmail.com>
2014-06-14 01:51:51 +0800

    admin/product: add price

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140611125349) do
+ActiveRecord::Schema.define(version: 20140613173818) do
 
   create_table "photos", force: true do |t|
     t.integer  "product_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140611125349) do
     t.integer  "quantity"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "price"
   end
 
   create_table "users", force: true do |t|
