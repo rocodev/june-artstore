c8b3c55f35ee4322f9af490305b3e0e94e6dda39
orga <orga.shih@gmail.com>
2014-06-14 01:28:15 +0800

    admin/products: add edit, destroy and update actions

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :admin_required
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
 
   def index
     @products = Product.all
@@ -12,9 +13,8 @@ class Admin::ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
-  
+
   def create
     @product = Product.new(product_params)
 
@@ -25,7 +25,28 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to admin_product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to admin_products_path
+  end
+
   private
+  def find_product
+    @product = Product.find(params[:id])
+  end
+
   def product_params
     params.require(:product).permit(:title, :description, :quantity, :photos_attributes => [:image])
   end
--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -1,9 +1,2 @@
 module Admin::ProductsHelper
-  def products_render_image
-    if @product.photos.first != nil
-      image_tag @product.photos.first.image.url, :class => "img-rounded"
-    else
-      "No Pic"
-    end
-  end
 end
