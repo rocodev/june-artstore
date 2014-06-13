08c7a71a010b03f38dce43eb3413e56c072e3015
Szu <szu@Szu-Air.local>
2014-06-13 12:55:22 +0800

    Add navbar and login/logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  
+  def admin_required
+  	current_user.admin?
+  end
+
 end
