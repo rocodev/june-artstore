e2e0c16abf3e6a61a2017cf5a0e118daf45bc6e4
Szu <szu@Szu-Air.local>
2014-06-13 20:52:14 +0800

    整理縮排

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,55 +1,51 @@
 class Admin::ProductsController < ApplicationController
-	
-	before_action :authenticate_user!
-	before_action :admin_required
-	before_action :set_product, only: [:show, :edit, :update, :destroy]
 
-	def index
-		@products = Product.all
-	end
+  before_action :authenticate_user!
+  before_action :admin_required
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
-	def new
-		@product = Product.new
-	end
+  def index
+    @products = Product.all
+  end
 
-	def create
-		@product = Product.new(product_params)
+  def new
+    @product = Product.new
+  end
 
-		if @product.save
-			redirect_to admin_products_path
-		else
-			render :new
-		end
-	end
+  def create
+    @product = Product.new(product_params)
 
-
-	def show
+    if @product.save
+      redirect_to admin_products_path
+    else
+      render :new
     end
+  end
 
-	def edit
-		
-    end
-	
-	def update
-		@product.update_attributes(product_params)
-		redirect_to admin_products_path
-    end
+  def show
+  end
 
-    def destroy
-    	@product.destroy
-    	redirect_to admin_products_path
-    end
-    
-	private  #private behind this line 
+  def edit
+  end
 
+  def update
+    @product.update_attributes(product_params)
+    redirect_to admin_products_path
+  end
 
-    def set_product
-      @product = Product.find(params[:id])
-    end
+  def destroy
+    @product.destroy
+    redirect_to admin_products_path
+  end
 
-	def product_params
-		params.require(:product).permit(:title, :description, :quanttity, :picture)
-	end
+  private  #private behind this line
 
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:title, :description, :quanttity, :picture)
+  end
 
 end
