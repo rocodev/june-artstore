1c5d61d9ba01509e7f811361574f7bf8be54298b
myself162 <myself162@hotmail.com>
2014-06-13 15:10:12 +0800

    gitignore public/upload; fix css issue

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
+  belongs_to :user
+
+  validates :title, :presence => true
+  validates :image, :presence => true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :products
 
   def admin?
     is_admin
