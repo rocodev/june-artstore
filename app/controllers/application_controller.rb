e2e5cabcabc2bbce76319afccce49af6aeeea88a
orga <orga.shih@gmail.com>
2014-06-14 10:34:13 +0800

    only admin can get into management page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def admin_required
-    current_user.admin?
+    unless (current_user && current_user.is_admin?)
+      redirect_to root_path, :flash => { :error => "no permission" }
+    end
   end
 end
