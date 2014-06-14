7eafa76674c844f9bee026490e0e9268532af0d6
Vincent Chen <vince78718@gmail.com>
2014-06-14 13:18:36 +0800

    admin can only edit its owned product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ActiveRecord::Base
 	belongs_to :creater, :class_name => "User", :foreign_key => :user_id
 
 	def editable_by?(user)
-		user && user == :creater
+		user && user == creater
 	end
 end
