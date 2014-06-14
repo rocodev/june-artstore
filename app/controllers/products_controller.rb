aaff06439a051a7543862dae9cbb18d47f2f992a
humanhighway <humanhighway@gmail.com>
2014-06-14 18:02:44 +0800

    add product index

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,11 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.order 'id desc'
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'pages#index'
+  root 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -59,5 +59,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
+
+  resources :products
+
+
 
 end
