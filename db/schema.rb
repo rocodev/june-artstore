16e6710c89bc9c4154ce4ff46221c1c40435c12c
Vincent Chen <vince78718@gmail.com>
2014-06-14 23:07:55 +0800

    set default values of price adn quantity of product

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140614043612) do
+ActiveRecord::Schema.define(version: 20140614150225) do
 
   create_table "products", force: true do |t|
     t.string   "name"
     t.text     "description"
-    t.decimal  "price"
-    t.integer  "quantity"
+    t.decimal  "price",       default: 0.0
+    t.integer  "quantity",    default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
