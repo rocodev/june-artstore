fbd6a35579f2e1495a304521df4b242c4967d3ad
Whien <sal95610@gmail.com>
2014-06-15 02:40:14 +0800

    add product#index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,9 @@
 class ProductsController < ApplicationController
+	
+	def index
+		@products = Product.all
+	end
+
 	def show
 		@product = Product.find(params[:id])
 		@photo = Photo.find_by(:product_id => @product.id)
