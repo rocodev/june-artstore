f57a7b13f43eb64a39825af06fda47e5b98e23a5
j0c <joe@junctiontree.com>
2014-06-15 08:27:52 +0800

    Verions #1, carrierwave doesn't work correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +55,11 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  namespace :admin do
+	  resources :products
+  end
+
+  resources :products
+
+  root to: 'products#index'
 end
