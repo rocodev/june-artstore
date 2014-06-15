01d5e414fb221ef224bfcb997e67beae7c141e83
humanhighway <humanhighway@gmail.com>
2014-06-15 12:58:08 +0800

    add notice stickits

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,20 @@
 module ApplicationHelper
 
-  # for twitter bootstrap message
-  def flash_class(level)
-    case level
-      when 'notice' then "alert alert-info"
-      when 'success' then "alert alert-success"
-      when 'error' then "alert alert-danger"
-      when 'alert' then "alert alert-warning"
+  def notice_message
+    alert_types = { notice: :success, alert: :danger }
+    close_button_options = { class: 'close', "data-dismiss" => 'alert', 'aria-hidden' => true }
+    close_button = content_tag(:button, 'x', close_button_options)
+
+    alerts = flash.map do |type, message|
+      alert_content = close_button + message
+
+      alert_type = alert_types[type.to_sym] || type
+      alert_class = "alert alert-#{alert_type} alert-dismissable"
+
+      content_tag(:div, alert_content, class: alert_class)
     end
+
+    alerts.join("\n").html_safe
   end
 
 end
