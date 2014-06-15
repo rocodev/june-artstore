a5ac8de88504ae6900030b1f1c98bdbfb06fffdf
orga <orga.shih@gmail.com>
2014-06-15 13:09:54 +0800

    render image: using image instead of raw text of No Pic

--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -1,9 +1,11 @@
 module Admin::ProductsHelper
   def product_render_photo(product)
     if product.default_photo.present?
-      image_tag product.default_photo.image.url, :class => "img-rounded", :alt => product.title
+      image_url = product.default_photo.image.url
     else
-      "No Pic!"
+      image_url = "http://placehold.it/200x200&text=No Pic"
     end
+
+    image_tag image_url, :class => "img-rounded", :alt => product.title
   end
 end
