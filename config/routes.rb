ae1e0a69a7fda5a951f5359dba8b29a7637e3686
Jimmy Kuo <kuo.yangyang@gmail.com>
2014-06-15 20:27:47 +0800

    Added: class 1, init

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+
+  root to: 'admin/products#index'
+
+  devise_for :users
+  namespace :admin do
+    resources :products
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
