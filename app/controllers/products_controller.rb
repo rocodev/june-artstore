3045b18cd86a5b6d2624402a40fa15b6b2498cb2
FunnyQ <funnyq@funnyq.com>
2014-06-16 17:56:22 +0800

    update products view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,8 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all
+    # 依 ID 降冪排序（最新的產品在最上面）
+    @products = Product.order("id DESC")
   end
 
   def show
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,7 +4,7 @@ module ProductsHelper
     if photo.present? #產品是否有照片
       image_tag(photo.image.thumb.url)
     else
-      "no pic"
+      image_tag("http://placehold.it/250x250&text=No Pic")
     end
   end
 
