78bc6ee50a3c8933fee9718c3dfb11657a9d407f
FunnyQ <funnyq@funnyq.com>
2014-06-16 17:12:26 +0800

    後台增加 DELET 功能

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -35,6 +35,12 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path
+  end
+
   def index
     @products = Product.all
   end
