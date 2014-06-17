a58986532c9890adc9f9b16a656cdd8fb7508b16
FunnyQ <funnyq@funnyq.com>
2014-06-17 16:13:55 +0800

    update views and helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,11 +1,27 @@
 module ProductsHelper
 
-  def render_product_photo(photo)
-    if photo.present? #產品是否有照片
-      image_tag(photo.image.thumb.url)
+  def render_product_photo(photo, size = "thumb")
+
+    if photo.present?
+      image_url = photo.image.send(size).url
     else
-      image_tag("http://placehold.it/250x250&text=No Pic")
+
+     case size
+      when :medium
+         volume = "300x300"
+      else
+         volume = "200x200"
+      end
+
+      image_url = "http://placehold.it/#{volume}&text=No Pic"
     end
+
+    image_tag(image_url, :class => "thumbnail")
+
+  end
+
+  def render_product_name(product)
+    product.title
   end
 
 end
