7464ade8775d6104ba8da1522adec8d2c62f07e9
motephyr <motephyr@gmail.com>
2014-06-17 18:09:40 +0800

    patch cart&cart_item model method

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,8 @@
 class Cart < ActiveRecord::Base
-  has_many :cart_items
+  has_many :cart_items, :dependent => :destory
+  has_many :items, :through => :cart_items, :source => :product
+
+  def add_product_to_cart(product)
+    items << product
+  end
 end
