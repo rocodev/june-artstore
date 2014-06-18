2cb429ee2aebb536e87e4e1df28ded410638c9b6
FrankyWu <forefeel741114@gmail.com>
2014-06-18 14:27:32 +0800

    fix admin/show.html.erb

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -18,6 +18,10 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def index
     @products = Product.all
   end
