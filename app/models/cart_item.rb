0ffd6058cf63c776f197c7645d440e90c91490f5
orga <orga.shih@gmail.com>
2014-06-18 21:56:56 +0800

    user can't add product with 0 quantity into cart

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,14 @@ class CartItem < ActiveRecord::Base
   belongs_to :cart
   belongs_to :product
 
-  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 1}
+  validate :check_product_quantity
 
+  private
+  def check_product_quantity
+    
+    if product.quantity < quantity
+      errors.add(:quantity, "You order too many")
+    end
+  end
 end
