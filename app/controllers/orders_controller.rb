4b6b14d8bca180f13dc088f1d1fb30ec2cb2558a
FunnyQ <funnyq@funnyq.com>
2014-06-19 14:29:25 +0800

    generate random url for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,11 +8,18 @@ class OrdersController < ApplicationController
     if @order.save
       @order.build_item_cache_from_cart(current_cart)
       @order.caculate_total!(current_cart)
-      redirect_to root_path
+      # 產生 token 保密訂單網址
+      redirect_to order_path(@order.token)
     else
       # 跨 controller
       render "carts/checkout"
     end
+  end
+
+  def show
+    @order = current_user.orders.find_by_token(params[:id])
+    @order_info = @order.info
+    @order_items = @order.items
   end
 
   private
