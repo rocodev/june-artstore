45b32f863e90ba6e66e88dcf15d5abdfb7a135e9
Vincent Chen <vince78718@gmail.com>
2014-06-20 01:18:16 +0800

    Add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,14 +5,25 @@ class Order < ActiveRecord::Base
 
 	accepts_nested_attributes_for :info
 
+	brfore_create :generate_token
+
 	def build_item_cache_from_cart(cart)
 		cart.items.each do |cart_item|
-			item = item.build
+			item = items.build
 			item.product_name = cart_item.name
 			item.quantity = cart_item.quantity
 			item.price = cart_item.price
 			item.save
 		end
+	end
+
+	def calculate_total!(cart)
+		self.total = cart.total_price
+		self.save
+	end
+
+	def generate_token
+		self.token = SecureRandom.uuid
 	end
 
 end
