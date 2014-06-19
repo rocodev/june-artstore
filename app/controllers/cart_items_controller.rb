666f94de9b556afa3ccc3b4d3ca2a66519c8b43c
FunnyQ <funnyq@funnyq.com>
2014-06-19 23:37:13 +0800

    建立 destroy action, 更新按鈕連結，使用者可以從購物車內刪掉一件物品

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,2 +1,11 @@
 class CartItemsController < ApplicationController
+
+  def destroy
+    @cart = current_cart
+    @item = @cart.cart_items.find(params[:id])
+    @item.destroy
+    flash[:notice] = "已經將商品移出購物車"
+    redirect_to :back
+  end
+
 end
