ca8ad312d6376f9ea0a979c6b0866167c7a91715
xdite <xuite.joke@gmail.com>
2014-06-20 00:54:19 +0800

    ✔ 身為消費者，我可以從購物車內更新購買數量 @done (14-06-20 00:54)

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -13,7 +13,7 @@ class Cart < ActiveRecord::Base
   has_many :items, :through => :cart_items, :source => :product
 
 
-  def add_product_to_cart(product)
+  def add_product_to_cart(product, amount)
     cart_item = cart_items.build
     cart_item.product = product
     cart_item.quantity = amount
