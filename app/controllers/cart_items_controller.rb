bd2b53324f4d1539a46a40af663d54f379d0129e
Whien <sal95610@gmail.com>
2014-06-19 23:14:52 +0800

    add cart_item quantity change

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -7,4 +7,24 @@ class CartItemsController < ApplicationController
     else
     end
   end
+
+  def update
+    
+    @cart = Cart.find(params[:cart_id])
+    @cart_item = @cart.cart_items.find(params[:id])
+
+    if params[:cart_item]['quantity'].to_i <= 1
+      flash[:warning] = "數量無法小於0，請確認數量。"
+      redirect_to(carts_path)
+    else
+      if @cart_item.update(cart_item_params)
+        redirect_to(carts_path)
+      end
+    end
+
+  end
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
+  end
 end
