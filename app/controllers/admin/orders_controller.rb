23f593ad90832c0c504977f41707f3a382175e42
FunnyQ <funnyq@funnyq.com>
2014-06-20 14:19:24 +0800

    增加 admin 更改訂單選項

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,4 +5,17 @@ class Admin::OrdersController < ApplicationController
     @orders = Order.all.limit(10)
   end
 
+  def change_state_to(state)
+    case state
+    when "ship"
+      @orders.ship!
+    when "deliver"
+      @orders.deliver!
+    when "cancel"
+      @orders.cancel_order!
+    when "return"
+      @orders.return_good!
+    end
+  end
+
 end
