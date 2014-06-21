1b3580a2813d3ea9c7dc428060f0dbe57fd175c2
FrankyWu <forefeel741114@gmail.com>
2014-06-21 22:19:45 +0800

    身為消費者，在結賬完後，會自動跳到訂單的管理界面 @done (14-06-21 22:19)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,8 +15,6 @@ class OrdersController < ApplicationController
       @order.build_item_cache_from_cart(current_cart)
       @order.calculate_total!(current_cart)
 
-      current_cart.clear!
-
       redirect_to order_path(@order.token)
     else
       render "carts/checkout"
@@ -27,10 +25,12 @@ class OrdersController < ApplicationController
     @order = current_user.orders.find_by_token(params[:id])
     @order.set_payment_with!("credit_card")
 
+    current_cart.clear!
+
     #@order.pay! #TODO: implemnt pay method later
     @order.make_payment! #TODO: should move to stripe callbacks
 
-    redirect_to root_path, :notice => "成功完成付款"
+    redirect_to account_orders_path, :notice => "成功完成付款"
   end
 
   private
