916e4292f0d623f10e6f42c4ca2408783c07b2cb
Vincent Chen <vince78718@gmail.com>
2014-06-21 09:12:10 +0800

    user is able to checkout order

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,14 +5,14 @@ class Order < ActiveRecord::Base
 
 	accepts_nested_attributes_for :info
 
-	brfore_create :generate_token
+	before_create :generate_token
 
 	def build_item_cache_from_cart(cart)
-		cart.items.each do |cart_item|
+		cart.cart_items.each do |cart_item|
 			item = items.build
-			item.product_name = cart_item.name
+			item.product_name = cart_item.product.name
 			item.quantity = cart_item.quantity
-			item.price = cart_item.price
+			item.price = cart_item.product.price
 			item.save
 		end
 	end
@@ -24,6 +24,14 @@ class Order < ActiveRecord::Base
 
 	def generate_token
 		self.token = SecureRandom.uuid
+	end
+
+	def set_payment_with!(method)
+		self.update_column(:payment_method, method)
+	end
+
+	def pay!
+		self.update_column(:paid, true)
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140619171156) do
+ActiveRecord::Schema.define(version: 20140619172840) do
 
   create_table "cart_items", force: true do |t|
     t.integer  "cart_id"
@@ -47,11 +47,12 @@ ActiveRecord::Schema.define(version: 20140619171156) do
 
   create_table "orders", force: true do |t|
     t.integer  "user_id"
-    t.decimal  "total",      default: 0.0
-    t.boolean  "paid",       default: false
+    t.decimal  "total",          default: 0.0
+    t.boolean  "paid",           default: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "token"
+    t.string   "payment_method"
   end
 
   add_index "orders", ["token"], name: "index_orders_on_token"
