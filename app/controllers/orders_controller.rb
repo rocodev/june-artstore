31c254403ec90c758270a93340bec5705eaf1cda
Vincent Chen <vince78718@gmail.com>
2014-06-21 09:27:43 +0800

    after convert cart to order, clean up the cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,6 +14,8 @@ class OrdersController < ApplicationController
 			@order.build_item_cache_from_cart(current_cart)
 			@order.calculate_total!(current_cart)
 
+			current_cart.clear!
+
 			redirect_to order_path(@order.token)
 		else
 			# render carts controller 的 checkout action
