246cc2debaa51454928dbe9d3fa2a2a40f34a1e5
motephyr <motephyr@gmail.com>
2014-06-22 17:59:40 +0800

    use eval to change order state

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,4 +4,15 @@ class Admin::OrdersController < AdminController
     @orders = Order.all
   end
 
+  def update
+    @order = Order.find(params[:id])
+  
+    eval("@order.#{order_params[:aasm_state]}!")
+
+    redirect_to :back
+  end
+
+  def order_params
+    params.require(:order).permit(:aasm_state)
+  end
 end
