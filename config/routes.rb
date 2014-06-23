aae35fa085c85b23f11fea8bd672c824e7c9dc07
jakobcho <jakobcho@gmail.com>
2014-06-23 16:56:06 +0800

    home work#5 參考官方作業，但還有bug

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,19 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
     resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return 
+      end 
+    end
   end
 
+  namespace :account do
+    resources :orders
+  end
 
 
   resources :products do
