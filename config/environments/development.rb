ed20820474a898966896daf401390cf73b3d5875
orga <orga.shih@gmail.com>
2014-06-25 00:36:29 +0800

    add order link into mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,16 +16,16 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.delivery_method = :letter_opener
-  #config.action_mailer.smtp_settings = {
-  #  :port => 587,
-  #  :address => "smtp.mailgun.org",
-  #  :user_name => "postmaster@sandboxc429a9c3e2a6491da69d4593dc8fcf34.mailgun.org",
-  #  :password => "9dtq9y2j4n23",
-  #  :domain => "sandboxc429a9c3e2a6491da69d4593dc8fcf34.mailgun.org",
-  #  :authentication => :plain
-  #}
-  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.smtp_settings = {
+    :port => 587,
+    :address => "smtp.mailgun.org",
+    :user_name => "postmaster@sandboxc429a9c3e2a6491da69d4593dc8fcf34.mailgun.org",
+    :password => "9dtq9y2j4n23",
+    :domain => "sandboxc429a9c3e2a6491da69d4593dc8fcf34.mailgun.org",
+    :authentication => :plain
+  }
+  config.action_mailer.delivery_method = :smtp
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -45,4 +45,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
 end
