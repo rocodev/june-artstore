ef0e5167ea9dc5806db9e5be5ef7990f4a6d0270
xdite <xuite.joke@gmai.com>
2014-06-28 20:58:21 +0800

    extract to stripe charger service

--- app/controllers/card_charges_controller.rb
+++ app/controllers/card_charges_controller.rb
@@ -7,29 +7,21 @@ class CardChargesController < ApplicationController
     @order = current_user.orders.find_by_token(params[:order_id])
     @amount = @order.total * 100 # in cents
    
-    Stripe.api_key = Setting.stripe.secret_key
-
-    customer = Stripe::Customer.create(
-      :email => current_user.email,
-      :card  => params[:stripeToken]
-      )
-
-
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
+    charge = StripeCharge.create(
       :amount      => @amount,
+      :card  => params[:stripeToken],
       :description => @order.token ,
-      :currency    => 'usd'
     )
 
-    @order.set_payment_with!("credit_card")
-    @order.make_payment! 
-
-    redirect_to order_path(@order.token), :notice => "成功完成付款"
-
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
+    if charge.successful?
+      @order.set_payment_with!("credit_card")
+      @order.make_payment! 
+      redirect_to order_path(@order.token), :notice => "成功完成付款"
+    else
+      flash[:error] = charge.error_message
       render "orders/pay_with_credit_card"
+    end
+
   end
 
 end
---
+++ app/services/stripe_charge.rb
@@ -0,0 +1,31 @@
+class StripeCharge
+
+  attr_reader :error_message, :response
+
+  def initialize(options={})
+    @response = options[:response]
+    @error_message = options[:error_message]
+  end
+
+  def self.create(options={})
+
+    Stripe.api_key = Setting.stripe.secret_key
+    
+    begin
+      response = Stripe::Charge.create(
+        amount: options[:amount],
+        currency: "usd",
+        card: options[:card],
+        description: options[:description]
+        )
+      new(:response => response)
+    rescue Stripe::CardError => e
+      new(:error_message => e.message)
+    end
+  end
+
+  def successful?
+    @response.present?
+  end
+
+end
