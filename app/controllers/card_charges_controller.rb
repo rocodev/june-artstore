2521bae01dd81856b6a02c2b309c33e98217deba
Szu <szu@Szu-Air.local>
2014-06-29 12:19:57 +0800

    git ignore rvmrc && stripe object debug

--- app/controllers/card_charges_controller.rb
+++ app/controllers/card_charges_controller.rb
@@ -6,25 +6,8 @@ class CardChargesController < ApplicationController
 
     @order = current_user.orders.find_by_token(params[:order_id])
     @amount = @order.total * 100 # in cents
-    @token = params[:stripeToken]
-   
-    Stripe.api_key = Settings.stripe.test_secret_key
-
-    customer = Stripe::Customer.create(
-      :email => current_user.email,
-      :card  => params[:stripeToken]
-      )
-
-
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => '100',
-      :description => @token,
-      :currency    => 'usd'
-    )
-
-    @order.set_payment_with!("credit_card")
-    @order.make_payment! 
+    @params = params[:stripeToken]
+    CardChargesService.new(current_user, @order, @amount, @params).charge_card!
 
     redirect_to order_path(@order.token), :notice => "成功完成付款"
 
@@ -34,46 +17,3 @@ class CardChargesController < ApplicationController
   end
 
 end
-
-
-#class CardChargesController < ApplicationController
- 
-  #before_action :authenticate_user!
- 
-  #def create
- 
-    #@order = current_user.orders.find_by_token(params[:order_id])
-    #@amount = @order.total * 100 # in cents
-    #@params = params[:stripeToken]
-    #CardChargesService.new(current_user, @order, @amount, @params).charge_card!
-    #Stripe.api_key = Settings.stripe.test_secret_key
- 
-    # customer = Stripe::Customer.create(
-    #   :email => current_user.email,
-    #   :card  => params[:stripeToken]
-    #   )
-
-
-    # charge = Stripe::Charge.create(
-    #   :customer    => customer.id,
-    #   :amount      => @amount,
-    #   :description => @order.token ,
-    #   :currency    => 'usd'
-    # )
-
-
-
-
-    #@order.set_payment_with!("credit_card")
-    #@order.make_payment! 
-    #OrderMailer.notify_order_paid_by_credit(@order).deliver
-    #redirect_to order_path(@order.token), :notice => "成功完成付款"
-   
-    #rescue Stripe::CardError => e
-    #  flash[:error] = e.message
-    #  render "orders/pay_with_credit_card"
- # end
-
-
- 
-#end
